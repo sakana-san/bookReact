ff2d9f6e37c04d3fa141054e60db75928bd33eab
sakana-san <sakana@sakana-mac.local>
2017-10-15 18:20:15 +0900

    babelのtest.jsをリファクタリング

--- babel/test.js
+++ babel/test.js
@@ -1,32 +1,32 @@
 // BMI計算
-// 体重 / 身長**
+// 体重 / ((身長 * 2) / 100)
 
-class BMI {
-  constructor (height, weight) {
-    this.height = height
-    this.weight = weight
-    this.bmi = this.calc()
-  }
-  calc () {
-    return this.weight / ((this.height * 2) / 100)
-  }
-  print () {
-    let text = '普通'
-    if (this.bmi >= 25) {
-      text = '肥満'
-    } else if (this.bmi >= 1100) {
-      text = '普通'
-    } else {
-      text = '痩せすぎ'
-    }
-    console.log('BMI計算結果', this.bmi, text)
-  }
-}
+// class BMI {
+//   constructor (height, weight) {
+//     this.height = height
+//     this.weight = weight
+//     this.bmi = this.calc()
+//   }
+//   calc () {
+//     return this.weight / ((this.height * 2) / 100)
+//   }
+//   print () {
+//     let text = '普通'
+//     if (this.bmi >= 25) {
+//       text = '肥満'
+//     } else if (this.bmi >= 1100) {
+//       text = '普通'
+//     } else {
+//       text = '痩せすぎ'
+//     }
+//     console.log('BMI計算結果', this.bmi, text)
+//   }
+// }
 
 
-const result = new BMI(172, 68)
+// const result = new BMI(172, 68)
 
-result.print()
+// result.print()
 
 
 // 消費税
@@ -57,28 +57,27 @@ result.print()
 
 // 高校野球
 
-// class BASEBALL {
-//   constructor(Toin, Saibi) {
-//     this.data = {
-//       scoreA: Toin,
-//       scoreB: Saibi,
-//       name: {}
-//     }
-//     this.shaping(this.data.scoreA, this.data.scoreB)
-//   }
-//   shaping (t, s) {
-//     if (t > s) {
-//       return this.data.name = '大阪桐蔭'
-//     } else {
-//       return this.data.name = '済美'
-//     }
-//   }
-//   print () {
-//     let text = '本日の試合は' + this.data.name  + 'が' + this.data.scoreA + '対' + this.data.scoreB + 'で勝ちました';
-//     (this.data.scoreA > this.data.scoreB) ? text : text;
-//     console.log(text)
-//   }
-// }
-//
-// let result = new BASEBALL(6, 0)
-// result.print()
+class BASEBALL {
+  constructor(Toin, Saibi) {
+    this.data = {
+      scoreA: Toin,
+      scoreB: Saibi,
+      name: {}
+    }
+    this.shaping(this.data.scoreA, this.data.scoreB)
+  }
+  shaping (t, s) {
+    if (t > s) {
+      return this.data.name = '大阪桐蔭'
+    } else {
+      return this.data.name = '済美'
+    }
+  }
+  print () {
+    let text = '本日の試合は' + this.data.name  + 'が' + this.data.scoreA + '対' + this.data.scoreB + 'で勝ちました';
+    console.log(text)
+  }
+}
+
+let result = new BASEBALL(7, 4)
+result.print()
