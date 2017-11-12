b067a151b581a72ddfc5e6ab8e46c2a0a9dcd05c
sakana-san <sakana@sakana-mac.local>
2017-11-12 16:26:08 +0900

    郵便番号reactもリファクタリングしたった

--- react/cli/input/src/changeMessage.js
+++ react/cli/input/src/changeMessage.js
@@ -1,40 +1,51 @@
-import React from 'react';
+import React from 'react'
 
 export default class extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      code: this.props.switchCode,
-      label: '',
-      bColor: ''
+      value: '',
+      converted: '',
+      flag: false
     }
   }
-  componentWillReceiveProps(newProps) {
-    if (newProps.switchCode) {
-      this.setState({
-        code:  newProps.switchCode,
-        label: 'OK',
-        bColor: 'blue'
-      })
-    } else {
-      this.setState({
-        code:  '',
-        label: 'NG',
-        bColor: 'red'
-      })
+  handler (e) {
+    let target = e.target.value
+    let targetValue = target.replace(/[^0-9-]+/g, '')
+    let convertedZip = /^\d{3}-\d{4}$/
+    let shaping = convertedZip.test(targetValue)
+    this.setState({
+      value: targetValue,
+      converted: shaping,
+      flag: true
+    })
+  }
+  checkMessage() {
+    let notice = ''
+    let nullCheck = (this.state.value !== '') ? notice = 'NG' : notice = ''
+    if (this.state.flag) {
+      if (this.state.converted) {
+        notice = 'OK'
+      } else {
+        nullCheck
+      }
     }
+    return notice
   }
   render () {
+    let message = this.checkMessage()
     return (
-      <span
-        style={{
-          'marginLeft': '20px',
-          'padding': '40px',
-          'backgroundColor': this.state.bColor
-        }}
-      >
-        { this.state.label}
-      </span>
+      <div>
+        <h3>{ this.props.label }</h3>
+        <input
+          type="text"
+          value={ this.state.value }
+          onChange={(e) => {
+            this.handler(e)
+          }}
+        />
+        <span>{ message }</span>
+      </div>
     )
   }
 }
--- react/cli/input/src/input.js
+++ react/cli/input/src/input.js
@@ -1,32 +1,18 @@
-import React from 'react';
-import ChangeMessage from './changeMessage';
+import React from 'react'
+import ChangeMessage from './changeMessage'
 
 export default class extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      code: ''
-    }
-  }
-  handler (e) {
-    let numberShaping = e.target.value.replace(/[^0-9-]+/g, '')
-    let shaping = /^\d{3}-\d{4}$/
-    this.setState({
-      code: shaping.test(numberShaping),
-    })
   }
   render () {
-    return(
+    return (
       <div>
-        <p>郵便番号</p>
-        <input
-          type="text"
+        <ChangeMessage
+          label="郵便番号"
           onChange={(e) => {
             this.handler(e)
           }}
-        />
-        <ChangeMessage
-          switchCode={this.state.code}
         />
       </div>
     )
