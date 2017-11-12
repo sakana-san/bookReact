762cbe2a6a0d4315f72231553eccd32a48be2602
sakana-san <sakana@sakana-mac.local>
2017-11-12 15:52:00 +0900

    複数の場合のforminputチェックをリファクタリングした

--- react/cli/input/src/multi/FormInput.js
+++ react/cli/input/src/multi/FormInput.js
@@ -1,81 +1,76 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 export default class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      type: this.props.type,
-      name: this.props.name,
-      value: this.props.value,
+      value: '',
+      convated: '',
       flag: false
     }
   }
   // プロパティの型を定義 (defaultの中に記述するにはstaticが必要)
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    value: PropTypes.string,
+  static PropTypes = {
+    value: PropTypes.string.isRequired,
+    convated: PropTypes.string,
     onChange: PropTypes.func
   }
-
   // プロパティの初期値を定義 (defaultの中に記述するにはstaticが必要)
   static defaultProps = {
     value: '',
+    convated: '',
     onChange: null
   }
-  handler (e) {
-    let target = e.target.value
-    let name = e.target.name
-    let shaping = this.checkValue(target, name)
+  handler(e) {
+    let tValue = e.target.value
+    let tName = e.target.name
+    // メールのパターンをチェックする
+    let emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    // 電話番号のパターンチェック
+    let phonePattern = /^[0-9-()+]+$/
+    // 英数字しか入れないようにする。不要な文字は削除
+    let filterValue = (tName === 'email') ? tValue.replace(/[^\u0020-\u007e]+/g, '') : tValue.replace(/[^0-9-()+]/g, '')
+    // filterValueが、パターンの正規表現とマッチすればtrueを返す
+    let convatedValue = (tName === 'email') ? emailPattern.test(filterValue) : phonePattern.test(filterValue)
     this.setState({
-      value: shaping,
+      value: filterValue,
+      convated: convatedValue,
       flag: true
+
     })
+    // チェックした情報をinput.jsに伝達する。input.jsでは、この情報を使用して送信チェックする
     this.props.onChange({
-      dispatchValue: target,
-      dispatchShaping: shaping,
-      dispatchName: name
+      dispatchName: tName,
+      dispatchValue: filterValue,
+      dispatchConvated: convatedValue
     })
-  }
-  checkValue (target, name) {
-    let targetValue = target
-    if (name === 'email') {
-      targetValue.replace(/[^\u0020-\u007e]+/g, '')
-      let emailShaping = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-      return emailShaping.test(targetValue)
-    } else if (name === 'tel') {
-      targetValue.replace(/[^0-9-()]+/g, '')
-      let telShaping = /^[0-9-()+]+$/
-      return telShaping.test(targetValue)
     }
-  }
-  checkMessage () {
-    let notice = null
-    if (this.state.flag === true) {
-      if (this.state.value) {
-        notice = <span>OK</span>
+  checkMessage() {
+    let check = ''
+
+    let nullCheck = this.state.value !== '' ?  check = 'NG' : check = ''
+    // 最初非表示。テキストが入力されると表示される。
+    // convatedとマッチしれればOKを返す。
+    // そうじゃなければNGを返す。かつ、空白なら非表示。
+    if (this.state.flag) {
+      if (this.state.convated) {
+        check = 'OK'
       } else {
-        notice = <span>NG</span>
+        nullCheck
       }
-      return notice
     }
-  }
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      type: newProps.type,
-      name: newProps.name,
-      value: newProps.value
-    })
+    return check
   }
   render () {
     let message = this.checkMessage()
     return (
       <div>
-        <h3>{ this.props.title }</h3>
+        <h3>{ this.props.label }</h3>
         <input
-          type={ this.state.text }
-          name={ this.state.name }
+          type="text"
+          name={ this.props.name }
+          value={ this.state.value }
           onChange={(e) => {
             this.handler(e)
           }}
--- react/cli/input/src/multi/input.js
+++ react/cli/input/src/multi/input.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import FormInput from './FormInput';
+import React from 'react'
+import FormInput from "./FormInput"
 
 
 export default class extends React.Component {
@@ -7,52 +7,67 @@ export default class extends React.Component {
     super(props)
     this.state = {
       email: '',
-      tel: '',
-      allOk: false
+      phone: '',
+      flag: {
+        allOk: false,
+        emailOk: false,
+        phoneOk: false,
+        allNg: false
+      }
     }
-    this.oks = {}
+    this.result = {}
+    this.isOks = {}
+    this.flag = {}
   }
   handler (e) {
-    this.oks[e.dispatchName] = e.dispatchShaping
+    // isOksオブジェクトにe.dispatchNameという箱を作り、そこにe.dispatchConvatedを代入する
+    // flagの制御に使う
+    this.isOks[e.dispatchName] = e.dispatchConvated
+    // resultオブジェクトにe.dispatchNameという箱を作り、そこにe.dispatchValueを代入する
+    // 送信クリック時の表示に使う
+    this.result[e.dispatchName] = e.dispatchValue
     this.setState({
-      [e.dispatchName]: (this.oks[e.dispatchName]) ? e.dispatchValue : '',
-      allOk: this.oks['email'] && this.oks['email']
+      [e.dispatchName]: e.dispatchValue,
+      flag: {
+        allOk: (this.isOks['email'] && this.isOks['phone']),
+        emailNg: (!this.isOks['email'] && this.isOks['phone']),
+        phoneNg: (this.isOks['email'] && !this.isOks['phone']),
+        allNg: (!this.isOks['email'] && !this.isOks['phone'])
+      }
     })
   }
   submitHandler () {
-    if (this.state.allOk === true) {
-      window.alert(JSON.stringify(this.state))
+    if (this.state.flag.allOk) {
+      window.alert(JSON.stringify( this.result ))
+    } else if (this.state.flag.emailNg) {
+      window.alert('メールを入力してください')
+    } else if (this.state.flag.phoneNg) {
+      window.alert('電話番号を入力してください')
     } else {
-      window.alert('正しく入力してください')
+      window.alert('両方入力してください')
     }
   }
   render () {
-    return(
+    return (
       <div>
-        <form onSubmit={() => {
-          this.submitHandler()
+        <form onSubmit={(e) => {
+          this.submitHandler(e)
         }}>
-          <h2>複数の場合</h2>
           <FormInput
-            title="メール:"
-            type="text"
+            label="メール"
             name="email"
-            value={ this.state.email }
             onChange={(e) => {
               this.handler(e)
             }}
           />
-          <span> { this.state.label } </span>
           <FormInput
-            title="電話番号:"
-            type="number"
-            name="tel"
-            value={ this.state.tel }
+            label="電話"
+            name="phone"
             onChange={(e) => {
               this.handler(e)
             }}
           />
-          <input type="submit" />
+          <input type="submit" value="送信"/>
         </form>
       </div>
     )
