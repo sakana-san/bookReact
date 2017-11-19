09ec325c7af64cac30d6dcc1fc158bfabea8c265
sakana-san <sakana@sakana-mac.local>
2017-11-19 16:59:30 +0900

    reactでsuperagentを実装した（基本編）

--- react/cli/ajax/src/ajax.js
+++ react/cli/ajax/src/ajax.js
@@ -7,30 +7,44 @@ export default class extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-       items: {}
+      items: null,
+      flag: false
     }
   }
   componentWillMount () {
     request.get('./toin.json')
-      .end(this.callbackGet())
+      .accept('application/json')
+      .end((err, res) => {
+        this.loadJSON(err, res)
+      })
   }
-  callbackGet (err, res) {
+  loadJSON (err, res) {
     if (err) {
+      console.log(err)
       return
     }
-    console.log(res)
-    // this.setState({
-    //   items: res.body
-    // })
+    this.setState({
+      items: res.body,
+      flag: true
+    })
   }
   render () {
-    // let options = this.state.items.map((value, index) => {
-    //   console.log(value)
-    // })
+    let options = ''
+    if (!this.state.flag) {
+       return <div className="APP">現在読み込み中</div>
+    } else {
+      options = this.state.items.map((value, index) => {
+        return (
+          <option value={ value.number } key={ index }>
+            { value.name }
+          </option>
+        )
+      })
+    }
     return (
       <div>
         <div className="App">
-          強豪校: <select>テスト</select>
+          強豪校: <select>{ options }</select>
         </div>
       </div>
     )
